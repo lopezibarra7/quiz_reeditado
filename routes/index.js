7771133f63ee566be65513b969cfd56d1e8f788a
lopezibarra7 <lopezibarra7@gmail.com>
2015-07-10 12:50:47 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var quizController = requier('../controllers/quiz_controller');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'QUIZ rediseñado' });
+  res.render('index', { title: 'QUIZ rediseñado.' });
 });
 
 router.get('/author',function(req,res,next){
-	res.render('author',{title: 'APARTADO DE DATOS DEL AUTOR'})
+	res.render('author',{title: 'APARTADO DE DATOS DEL AUTOR.'})
 });
+
+router.get('/quizes/question',quizController.question);
+router.get('/quizes/answer',quizController.answer);
 
 module.exports = router;
