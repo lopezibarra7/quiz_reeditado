9ec3378e9b7c74f844c1bd22f601ec56cac19471
lopezibarra7 <lopezibarra7@gmail.com>
2015-07-15 16:30:39 +0200

    Filtro busqueda preguntas

---
+++ models/models.js
@@ -0,0 +1,45 @@
+var path = require ('path');
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
+//Cargar modelo ORM
+var Sequelize = require ('sequelize');
+
+var sequelize = new Sequelize (DB_name,user,pwd,
+{
+	dialect: protocol,
+	protocol: protocol,
+	port: port,
+	host: host,
+	storage: storage,
+	omitNull: true
+});
+
+//Usar BBDD SQLite
+//var sequelize = new Sequelize (null, null, null, {dialect: 'sqlite',storage:'quiz.sqlite'});
+
+//Importar la definicion de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+exports.Quiz = Quiz; //Exportamos definicion de tabla Quiz.
+
+sequelize.sync().then(function(){ // Creamos e inicializamos la bbdd
+	Quiz.count().then(function (count){
+		if (count === 0) {
+			Quiz.create({ 
+				pregunta: 'Capital de España',
+				respuesta: 'Madrid'});
+			Quiz.create({
+				pregunta: 'Capital de Portugal',
+				respuesta: 'Lisboa'})
+			.then(function(){console.log('Base de datos inicializada.')});
+		};
+	});
+});1
--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'QUIZ rediseñado.' });
 });
 
+router.param('quizId',quizController.load);
+
 router.get('/author',function(req,res,next){
 	res.render('author',{title: 'APARTADO DE DATOS DEL AUTOR.'})
 });
 
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer',quizController.answer);
+router.get('/quizes',	quizController.index);
+router.get('/quizes/:quizId(\\d+)',	quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
+router.get('/quizes/listado',quizController.listado);
+/*router.get('/quizes/question',quizController.question);
+router.get('/quizes/answer',quizController.answer);*/
 
 module.exports = router;
