c80b4e3678278f8e8a13f6ea1bc0fd85e13b8717
lopezibarra7 <lopezibarra7@gmail.com>
2015-07-15 16:30:39 +0200

    Filtro busqueda preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,11 +1,44 @@
-exports.question = function(req,res){
-	res.render('quizes/question',{pregunta:'Capital de España'})
+var models = require ('../models/models.js');
+
+exports.load = function (req,res,next,quizId){
+	models.Quiz.find(quizId).then(
+		function(quiz){
+			if(quiz){
+				req.quiz = quiz;
+				next();
+			} else {next(new Error('No existe quizId='+quiz.Id));}
+		}
+	).catch(function(error){next(error);});
 };
 
+
+exports.listado = function(req,res){
+	models.Quiz.findAll({ where:["pregunta LIKE ?",'%'+req.query.patron+'%']}).then(function(quizes){
+		res.render('quizes/listado.ejs',{quizes:quizes});
+	})
+};
+
+exports.show = function(req,res){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		res.render('quizes/show.ejs',{quiz:req.quiz})
+	})
+};
+
+exports.index = function (req,res){
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index.ejs',{
+			quizes:quizes
+		});
+	})
+};
+
+//GET /quizes/answer
 exports.answer = function(req,res){
-	if(req.query.respuesta === 'Madrid'){
-		res.render('quizes/answer',{respuesta:'Respuesta correcta.'});
-	}else {
-		res.render('quizes/answer',{respuesta:'Respuesta incorrecta. Vuelva a intentarlo.'});
-	}
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+			if(req.query.respuesta === req.quiz.respuesta){
+				res.render('quizes/answer.ejs',{quiz:req.quiz, respuesta:'Respuesta correcta.'});
+			}else {
+				res.render('quizes/answer.ejs',{quiz:req.quiz, respuesta:'Respuesta incorrecta. Vuelva a intentarlo.'});
+			}
+	})
 };
